91f65d5f52a8c1617dc1d555189db786f8740662
xinleima <1376260753@qq.com>
2017-05-02 20:36:04 +0800

    add line function in google.vue

--- src/views/google.vue
+++ src/views/google.vue
@@ -2,6 +2,7 @@
   <div class="wrapper wrapper-content animated fadeInRight">
     <nav class="navbar navbar-default">
       <form class="form-inline">
+        <label>mapId: {{mapId}}</label>
         <select v-model="curLayerId"  class="form-control">
           <option value="0">选择图层</option>
           <option v-for="selectLayer in selectLayers" v-bind:value="selectLayer.id">
@@ -33,22 +34,24 @@
         </ul>
       </span>
 
-        <span class="dropdown" v-if="curLayerType==='XSG'">
+        <span class="dropdown" v-if="curLayerType==='XSG' && curPoint===null">
         <button class="btn btn-default dropdown-toggle" type="button" id="createLineMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
           添加线
           <span class="caret"></span>
         </button>
         <ul class="dropdown-menu" aria-labelledby="createLineMenu">
-          <li><a v-on:click="addLine">添加好线</a></li>
-          <li><a v-on:click="addLine">添加坏线</a></li>
+          <li><a v-on:click="addGoodLine">添加好线</a></li>
+          <li><a v-on:click="addBadLine">添加坏线</a></li>
         </ul>
       </span>
+
+        <button type="button" v-on:click="stopAddLine" class="btn btn-default" v-if="curLayerType==='XSG' && curPoint!==null">停止</button>
 
         <button type="button" v-on:click="submitChange" class="btn btn-info">提交</button>
       </form>
     </nav>
 
-    <nav class="navbar navbar-default" id="pointNav" v-if="curPoint">
+    <nav class="navbar navbar-default" id="pointNav" v-if="curPoint && curLayerType==='YJG'">
       <form class="form-inline">
         <div class="form-group form-group-sm ">
           <label>lng</label>
@@ -67,6 +70,39 @@
         <button type="button" v-on:click="deletePointBtnClick" class="btn btn-danger">删除当前点</button>
       </form>
     </nav>
+    <nav class="navbar navbar-default" id="lineNav" v-if="curLine">
+      <form class="form-inline">
+        <div class="form-group form-group-sm ">
+          <label>lng</label>
+          <input type="text" v-model="lng" class="form-control inputPos" v-on:keyup="lngLatOnChange">
+        </div>
+        <div class="form-group form-group-sm">
+          <label>lat</label>
+          <input type="text" v-model="lat" class="form-control inputPos" v-on:keyup="lngLatOnChange">
+        </div>
+        <div class="form-group form-group-sm ">
+          <label>lng2</label>
+          <input type="text" v-model="lng2" class="form-control inputPos" v-on:keyup="lngLat2OnChange">
+        </div>
+        <div class="form-group form-group-sm">
+          <label>lat2</label>
+          <input type="text" v-model="lat2" class="form-control inputPos" v-on:keyup="lngLat2OnChange">
+        </div>
+
+        <!--<span><label>lat:</label> {{lat}}</span>-->
+        <!--<span><label>lng:</label> {{lng}}</span>-->
+        <!--<span><label>lat2:</label> {{lat2}}</span>-->
+        <!--<span><label>lng2:</label> {{lng2}}</span>-->
+
+
+        <select v-model="curPointStatus" class="form-control">
+          <option value="0">选择状态</option>
+          <option value="GOOD">状态GOOD</option>
+          <option value="BAD">状态BAD</option>
+        </select>
+        <button type="button" v-on:click="deleteLineBtnClick" class="btn btn-danger">删除当前线</button>
+      </form>
+    </nav>
     <div class="ibox-content" id="map" style="position: relative; height: 820px"></div>
   </div>
 </template>
@@ -82,6 +118,8 @@
 
         lng: "lng",
         lat: "lat",
+        lng2: "lng2",
+        lat2: "lat2",
         selectLayers:[],
 
         // 当前layer id以及map数据
@@ -92,7 +130,9 @@
         // 当前point 以及它的状态
         curPointStatus:0,
         curPoint: null,
-        prePoint: null
+        curLine: null,
+        tmpPoint: null,
+        mapClickListener: null
       }
     },
     methods: {
@@ -104,7 +144,6 @@
         });
       },
       getLayerDatas:function (mapId) {
-          console.log(mapId);
         var self = this;
         this.$http.get('http://localhost:8080/layer/layers?mapId='+mapId,
           {
@@ -199,8 +238,36 @@
           draggable:true
         });
       },
+      createPointDetail: function (pointPos, pointStatus) {
+        var cityCircle = this.createPoint(this.map,pointPos,this.getColorWithStatus(pointStatus));
+        cityCircle.pointStatus = pointStatus;
+        this.curLayerMapDatas.push(cityCircle);
+
+        google.maps.event.addListener(cityCircle,'drag',function () {
+          self.lng = cityCircle.center.lng().toFixed(6);
+          self.lat = cityCircle.center.lat().toFixed(6);
+        });
+        this.addPointClickListener(cityCircle);
+      },
+      createLine: function (map, pointList, color) {
+        var flightPath = new google.maps.Polyline({
+          path: pointList,
+          geodesic: true,
+          strokeColor: color,
+          strokeOpacity: 1.0,
+          strokeWeight: 10,
+          editable: true
+        });
+
+        flightPath.setMap(map);
+        this.addLineClickListener(flightPath);
+        return flightPath;
+      },
 
       //根据点的状态创建一个点
+      getColorWithStatus: function (status) {
+        return status==="GOOD"?"black":'#FF0000'
+      },
       addPoint:function (pointStatus) {
         var self = this;
         var mapClickListener = google.maps.event.addListener(this.map, 'click', function(event) {
@@ -208,16 +275,7 @@
           self.lng = latLng.lng().toFixed(6);
           self.lat = latLng.lat().toFixed(6);
 
-          var cityCircle = self.createPoint(self.map,latLng,pointStatus==="GOOD"?"black":'#FF0000');
-          cityCircle.pointStaus = pointStatus;
-          self.curLayerMapDatas.push(cityCircle);
-
-          google.maps.event.addListener(cityCircle,'drag',function () {
-            self.lng = cityCircle.center.lng().toFixed(6);
-            self.lat = cityCircle.center.lat().toFixed(6);
-          });
-          self.addPointClickListener(cityCircle);
-
+          self.createPointDetail(latLng, pointStatus);
           google.maps.event.removeListener(mapClickListener);
         });
       },
@@ -229,8 +287,43 @@
       },
 
       // 添加线
-      addLine: function () {
+      addLine: function (lineStatus) {
+        var self = this;
+        self.mapClickListener = google.maps.event.addListener(this.map, 'click', function(event) {
+          var latLng = event.latLng;
+          let tmpPoint = {lng:latLng.lng(), lat:latLng.lat()}
+          tmpPoint.lineStatus = lineStatus;
+          self.lng = latLng.lng().toFixed(6);
+          self.lat = latLng.lat().toFixed(6);
+          if(self.curPoint){
+            let curLine = self.createLine(self.map, [self.curPoint, tmpPoint],self.getColorWithStatus(lineStatus));
+            self.curLayerMapDatas.push(curLine);
+            if(self.tmpPoint){
+              self.tmpPoint.setMap(null);
+              self.tmpPoint = null;
+            }
+          }
+          else{
+            self.tmpPoint= self.createPoint(self.map, latLng, self.getColorWithStatus(lineStatus));
+          }
+          self.curPoint = tmpPoint;
+        });
+      },
+      addGoodLine: function () {
+        this.addLine("GOOD");
+      },
+      addBadLine: function () {
+        this.addLine("BAD");
+      },
 
+      stopAddLine: function () {
+        google.maps.event.removeListener(this.mapClickListener);
+        this.mapClickListener = null;
+        if(this.tmpPoint!==null){
+          this.tmpPoint.setMap(null);
+          this.tmpPoint = null;
+        }
+        this.curPoint = null;
       },
 
       // 根据layer id得到某一图层的数据
@@ -245,16 +338,34 @@
 
       //提交当前图层的更改
       submitChange: function () {
-        var curLayerData = this.getLayerData(this.curLayerId);
-        curLayerData.pointList =  [];
+        var curLayerData = null;
+        var self = this;
+        var patchUrl = null;
+        curLayerData = {type:self.curLayerType, pointList:[], lineList: []};
         this.curLayerMapDatas.forEach(function (curData) {
-          if(curLayerData.type==="YJG"){
+          if(self.curLayerType==="YJG"){
             curLayerData.pointList.push({
               x:curData.center.lng(),
               y:curData.center.lat(),
               z:0,
-              status:curData.pointStaus
+              status:curData.pointStatus
             });
+            patchUrl = "http://localhost:8080/layer/layers/point/id";
+          }
+          else if(self.curLayerType==="XSG"){
+            let vertices = curData.getPath();
+            let xy1 = vertices.getAt(0);
+            let xy2 = vertices.getAt(1);
+            curLayerData.lineList.push({
+              x: xy1.lng(),
+              y: xy1.lat(),
+              z: 0,
+              x2: xy2.lng(),
+              y2: xy2.lat(),
+              z2: 0,
+              status: curData.lineStatus
+            });
+            patchUrl = "http://localhost:8080/layer/layers/line/id";
           }
         });
         var postData = {
@@ -262,17 +373,17 @@
           data:curLayerData
         }
 
-        this.$http.patch('http://localhost:8080/layer/layers/point/id',
-          postData
-        ).then(function (response){
-          let responseBody = response.body
-          if (responseBody.code === 200) {
-            alert("提交成功！");
-          }
-          else{
-            alert("提交失败！");
-          }
-        });
+        this.$http.patch(patchUrl, postData)
+          .then(function (response){
+            let responseBody = response.body
+            if (responseBody.code === 200) {
+              self.getLayerDatas(self.mapId);
+              alert("提交成功！");
+            }
+            else{
+              alert("提交失败！");
+            }
+          });
       },
 
       // 为点添加动态效果
@@ -285,8 +396,8 @@
             self.curPoint.setMap(self.map);
           }
           self.curPoint = point;
-          self.curPointStatus = point.pointStaus||"BAD";
-          self.curPoint.strokeColor = self.curPoint.curPointStatus==="GOOD"?"black":'#FF0000';
+          self.curPointStatus = point.pointStatus||"BAD";
+          self.curPoint.strokeColor = self.getColorWithStatus(self.curPoint.curPointStatus);
           self.curPoint.strokeOpacity = 0.8;
           self.curPoint.strokeWeight = 2;
           self.curPoint.setMap(null);
@@ -295,17 +406,58 @@
           self.lat = point.center.lat().toFixed(6);
         });
       },
+      addLineClickListener: function (line) {
+        var self = this;
+        google.maps.event.addListener(line,'click',function () {
+          self.curLine = line;
+          self.curPointStatus = line.lineStatus||"BAD";
+          let vertices = line.getPath();
+          self.lat = vertices.getAt(0).lat().toFixed(6);
+          self.lng = vertices.getAt(0).lng().toFixed(6);
+          self.lat2 = vertices.getAt(1).lat().toFixed(6);
+          self.lng2 = vertices.getAt(1).lng().toFixed(6);
+        });
+        google.maps.event.addListener(line.getPath(), 'set_at', function () {
+          let vertices = line.getPath();
+          self.lat = vertices.getAt(0).lat().toFixed(6);
+          self.lng = vertices.getAt(0).lng().toFixed(6);
+          self.lat2 = vertices.getAt(1).lat().toFixed(6);
+          self.lng2 = vertices.getAt(1).lng().toFixed(6);
+        })
+      },
+
       lngLatOnChange: function () {
-        if(this.curPoint.center.lat()!==this.lat || this.curPoint.center.lng()!==this.lng){
+        if(this.curLayerType==="YJG") {
           this.curPoint.center = new google.maps.LatLng(this.lat, this.lng);
           this.curPoint.setMap(null);
           this.curPoint.setMap(this.map);
         }
+        else if(this.curLayerType==="XSG"){
+          let vertices = this.curLine.getPath();
+          vertices.removeAt(0);
+          vertices.insertAt(0, new google.maps.LatLng(this.lat, this.lng))
+          this.curLine.setMap(null);
+          this.curLine.setMap(this.map);
+        }
+      },
+
+      lngLat2OnChange: function(){
+        let vertices = this.curLine.getPath();
+        vertices.removeAt(1);
+        vertices.insertAt(1, new google.maps.LatLng(this.lat2, this.lng2))
+        console.log("xy2", vertices.getAt(1).lat(),vertices.getAt(1).lng());
+        this.curLine.setMap(null);
+        this.curLine.setMap(this.map);
       },
       deletePointBtnClick: function () {
         this.curPoint.setMap(null);
         this.removeListByValue(this.curLayerMapDatas,this.curPoint);
         this.curPoint = null;
+      },
+      deleteLineBtnClick: function () {
+        this.curLine.setMap(null);
+        this.removeListByValue(this.curLayerMapDatas,this.curLine);
+        this.curLine = null;
       },
 
 
@@ -323,38 +475,48 @@
       curLayerId: function(newValue, oldValue) {
         var self = this;
         this.clearMap(true);
-        console.log(newValue);
+
+        this.curPoint = null;
+        this.curLine = null;
+
         if(parseInt(newValue)=== 0){
-            this.curPoint = null;
-            this.curLayerType = 0;
-            return;
+          this.curLayerType = 0;
+          return;
         }
 
         var layerDatas = this.getLayerData(newValue);
         this.curLayerType = layerDatas.type;
-        this.curPoint = null;
 
         if(this.curLayerType==="YJG"){
           layerDatas.pointList.forEach(function (layerData) {
-            var cityCircle = self.createPoint(self.map,{lng:layerData.x,lat:layerData.y},
-              layerData.status==="GOOD"?"black":'#FF0000');
-            cityCircle.pointStaus = layerData.status;
-            self.curLayerMapDatas.push(cityCircle);
-            google.maps.event.addListener(cityCircle,'drag',function () {
-              self.lng = cityCircle.center.lng().toFixed(6);
-              self.lat = cityCircle.center.lat().toFixed(6);
-            });
-            self.addPointClickListener(cityCircle);
+            self.createPointDetail({lng:layerData.x,lat:layerData.y}, layerData.status);
+          });
+        }
+        else if(this.curLayerType==="XSG"){
+          layerDatas.lineList.forEach(function (layerData) {
+            let curLine = self.createLine(self.map,[
+              {lng:layerData.x, lat:layerData.y},
+              {lng:layerData.x2, lat:layerData.y2}
+            ],self.getColorWithStatus(layerData.status));
+            curLine.lineStatus = layerData.status;
+            self.curLayerMapDatas.push(curLine);
           });
         }
 
       },
       curPointStatus: function (newValue, oldValue) {
-        this.curPoint.pointStaus = newValue;
-        this.curPoint.fillColor = newValue==="GOOD"?"black":'#FF0000';
-        this.curPoint.setMap(null);
-        this.curPoint.setMap(this.map);
-
+        if(this.curLayerType==="YJG"){
+          this.curPoint.pointStatus = newValue;
+          this.curPoint.fillColor = this.getColorWithStatus(newValue);
+          this.curPoint.setMap(null);
+          this.curPoint.setMap(this.map);
+        }
+        else if(this.curLayerType === "XSG"){
+          this.curLine.lineStatus = newValue;
+          this.curLine.strokeColor = this.getColorWithStatus(newValue);
+          this.curLine.setMap(null);
+          this.curLine.setMap(this.map);
+        }
       }
     },
     mounted(){
@@ -372,5 +534,3 @@
     width:100px;
   }
 </style>
-
-
