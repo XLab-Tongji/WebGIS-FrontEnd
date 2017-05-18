5e70476afb58b298cafd32ccba636035d458256e
rain <iheyu22@gmail.com>
2017-05-18 17:08:27 +0800

    修正初始页面

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
-<div id="app-wrapper">
-  <sideBar></sideBar>
-  <div id="page-wrapper" class="gray-bg">
-    <headBar></headBar>
-    <router-view name="mainPage"></router-view>
-    <footBar></footBar>
-  </div>
-</div>
+  <login v-if="login == false"></login>
+  <logined v-else></logined>
 </template>
 
 <script>
+  import login from '@/views/login'
   import headBar from '@/views/bar/headBar'
   import sideBar from '@/views/bar/sideBar'
   import footBar from '@/views/bar/footBar'
 
   export default {
     name: 'app',
+    data () {
+      return {
+        login: false
+      }
+    },
     components: {
-      'headBar': headBar,
-      'sideBar': sideBar,
-      'footBar': footBar
+      'login': login,
+      'logined': {
+        template: '<div id="app-wrapper">' +
+        '<sideBar></sideBar>' +
+        '<div id="page-wrapper" class="gray-bg">' +
+        '<headBar></headBar>' +
+        '<router-view name="main"></router-view>' +
+        '<footBar></footBar>' +
+        '</div>' +
+        '</div>',
+        components: {'headBar': headBar,
+          'sideBar': sideBar,
+          'footBar': footBar}
+      }
     }
   }
 </script>
