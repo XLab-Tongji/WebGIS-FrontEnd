39ff0677f7717ecd8b5bbe90bb8c7424b50a88ba
LPP <lpp@LPPdeMacBook-Pro.local>
2017-06-01 17:16:07 +0800

    .

--- src/global.js
+++ src/global.js
@@ -3,7 +3,7 @@
  */
 
 // Server Address
-global.baseUrl = 'http://localhost:8080'         // for deploy
+// global.baseUrl = 'http://wb.lab-sse.cn:8080'         // for deploy
 // global.baseUrl = 'http://115.159.65.170:8080'   // for deploy plan B
 // global.baseUrl = 'http://localhost:8080'        // for local test
 
